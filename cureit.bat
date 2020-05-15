a519f89d31ec728a3ec1e07db18d72190ca49e9e
Bocharnikov Sergei <bocharnikov@dezigner.ru>
2020-05-15 20:11:34 +0600

    wget for windows

---
+++ cureit.bat
@@ -0,0 +1,4 @@
+:: wget для windows можно скачать здесь -> https://sourceforge.net/projects/gnuwin32/files/wget/1.11.4-1/
+
+cd C:\Program Files (x86)\GnuWin32\bin
+wget.exe -P C:\Users\Admin\Desktop --no-check-certificate --referer="https://free.drweb.ru/download+cureit+free/" --content-disposition "https://free.drweb.ru/download+cureit/gr/?lng=ru"
